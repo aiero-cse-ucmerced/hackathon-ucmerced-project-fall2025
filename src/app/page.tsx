d7235b566c8eb822303c3ed5b90d910cfa81d6eb
Joshua Coquia <mail@joshuacoquia.com>
2025-11-08 12:56:33 -0800

    feat: add hero banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import RootLayout from "./layout";
+import Image from "next/image";
+import HeroImage from "../images/IntelligentFlashcardsHero.png";
 
 export default function Home() {
   return (
     <RootLayout includeHeader>
       <p>Welcome to the Intelligent Flashcards App!</p>
+      <Image
+        src={HeroImage}
+        alt="Flashcards Illustration"
+        width={300}
+        height={200}
+      />
     </RootLayout>
   );
 }
